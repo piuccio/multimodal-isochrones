5e7560066595c77e08023edccc579f86d601fb8a
Fabio Crisci <piuccio@gmail.com>
2018-11-22 15:32:50 +0900

    Tiny optimization to remove unused code

--- benchmark/performance.js
+++ benchmark/performance.js
@@ -4,10 +4,9 @@ const pretty = require('pretty-ms');
 const iso = require('../index');
 const { performance } = require('perf_hooks');
 
+performance.mark('before');
 const graph = iso();
 json.edges.forEach((_) => graph.addEdge(_));
-
-performance.mark('before');
 graph.from('node_0').maxTime(60).maxLines(5).get();
 performance.mark('after');
 performance.measure('query', 'before', 'after');
