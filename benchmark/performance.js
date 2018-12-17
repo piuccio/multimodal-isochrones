a0d6ee729152f5a1fb71a6bc5d6d234819fa6922
Fabio Crisci <piuccio@gmail.com>
2018-12-17 15:48:03 +0900

    Change the performance metric to include intersect

--- benchmark/performance.js
+++ benchmark/performance.js
@@ -7,7 +7,10 @@ const { performance } = require('perf_hooks');
 performance.mark('before');
 const graph = iso();
 json.edges.forEach((_) => graph.addEdge(_));
-graph.from('node_0').maxTime(60).maxLines(5).get();
+graph.intersect(
+  graph.from('node_0').maxTime(60).maxLines(5),
+  graph.from('node_100').maxTime(60).maxLines(5),
+).get();
 performance.mark('after');
 performance.measure('query', 'before', 'after');
 const timeToQuery = performance.getEntriesByName('query')[0].duration;
